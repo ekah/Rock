18af529c324add9eac8261837474ced28fe19b45
mikepetersonccv <MikePeterson@ccvonline.com>
2013-06-10 09:34:48 -0700

    Add RockCoreConfiguration to migration configuration

--- Residency/Migrations/Configuration.cs
+++ Residency/Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 namespace com.ccvonline.Residency.Migrations
 {
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-
+    using System.Reflection;
+    
     internal sealed class Configuration : DbMigrationsConfiguration<com.ccvonline.Residency.Data.ResidencyContext>
     {
         public Configuration()
@@ -24,6 +26,17 @@ namespace com.ccvonline.Residency.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+    }
+
+    // If you update the Rock.dll often, this RockCoreConfiguration will help you do Update-Database for the Rock Core Tables
+    // To do this, run "Update-Database -ConfigurationTypeName:RockCoreConfiguration" using Package Manager Console
+    internal sealed class RockCoreConfiguration : DbMigrationsConfiguration<Rock.Data.RockContext>
+    {
+        public RockCoreConfiguration()
+        {
+            this.MigrationsAssembly = typeof( Rock.Data.RockContext ).Assembly;
+            this.MigrationsNamespace = "Rock.Migrations";
         }
     }
 }
